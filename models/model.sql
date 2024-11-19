8808dcd5d55609c7425aac983b28ea0976321861
Mike Driscoll <m.e.driscoll@gmail.com>
2024-11-19 00:58:26 -0800

    added union_by_name=true to handle schema mismatches

--- models/model.sql
+++ models/model.sql
@@ -3,7 +3,9 @@
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
 {{if dev}} 
-  SELECT * FROM 'gs://medriscoll-rill/rill-bluesky-jetstream/year=2024/month=11/day=18/hour=*/1731896872.parquet'
+  SELECT * FROM read_parquet('gs://medriscoll-rill/rill-bluesky-jetstream/year=2024/month=11/day=18/hour=06/1731910668.parquet',
+    union_by_name=true)
 {{else}}
-  SELECT * FROM 'gs://medriscoll-rill/rill-bluesky-jetstream/year=2024/month=11/day=18/hour=*/*.parquet'
+  SELECT * FROM read_parquet('gs://medriscoll-rill/rill-bluesky-jetstream/year=2024/month=11/day=18/hour=*/*.parquet',
+    union_by_name=true)
 {{end}}
