5496fd1bf6751e51f89a28d4f1f7d65468b95da9
Mike Driscoll <m.e.driscoll@gmail.com>
2024-11-19 01:02:24 -0800

    nudge

--- models/model.sql
+++ models/model.sql
@@ -5,6 +5,7 @@
 {{if dev}} 
   SELECT * FROM read_parquet('gs://medriscoll-rill/rill-bluesky-jetstream/year=2024/month=11/day=18/hour=06/1731910668.parquet',
     union_by_name=true)
+
 {{else}}
   SELECT * FROM read_parquet('gs://medriscoll-rill/rill-bluesky-jetstream/year=2024/month=11/day=18/hour=*/*.parquet',
     union_by_name=true)
